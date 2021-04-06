5c6b0863923f82c28054a864aeaa4d4dcc9f1ac8
Anug Saha <anugsaha@gmail.com>
2021-04-06 10:06:18 -0400

    greeting and greeting container

--- dappedup/frontend/entry.jsx
+++ dappedup/frontend/entry.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
+import Root from './components/root'
 import {login, signup, logout} from './utils/session';  
 
 document.addEventListener("DOMContentLoaded", () => {
 //   window.login = login; 
   const store= configureStore(); 
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to DAPPED UP!</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
