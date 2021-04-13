2c04c39335ebcd5422025fb34662ab15667904ae
Anug Saha <anugsaha@gmail.com>
2021-04-12 23:06:41 -0400

    add associations for post table

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
     after_initialize :ensure_session_token
     attr_reader :password
 
+    has_many :posts, 
+    foreign_key: :author_id, 
+    class_name: :Post
+    
     def self.find_by_credentials(email, password)
         user = User.find_by(email: email)
         return nil unless user
