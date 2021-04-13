c8d548ba6a3650b8b7b58dac6b73b66b8e93db60
Anug Saha <anugsaha@gmail.com>
2021-04-13 10:21:58 -0400

    add post contoller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :update, :show]
     resource :session, only: [:create, :destroy]
+    resources :posts, only: [:index, :create, :update, :show, :destroy]
   end 
   
   root "static_pages#root"
