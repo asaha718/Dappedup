6d9b67abd36fab63f57af9ca8c9ffae3eafd3699
Anug Saha <anugsaha@gmail.com>
2021-05-16 20:14:12 -0400

    create ajax calls for edu and job

---
+++ frontend/utils/education.js
@@ -0,0 +1,27 @@
+export const fetchEdus= ()=> (
+    $.ajax({ 
+        url: '/api/educations'
+    })
+); 
+
+export const fetchEdu=(eduId)=> ( 
+    $.ajax({ 
+        url: `/api/educations/${eduId}`
+    })
+); 
+
+export const createEdu= (edu)=> ( 
+    $.ajax({ 
+        url: '/api/education',
+        method: 'POST',
+        data: edu
+    })
+);
+
+export const updateEdu= (edu)=> ( 
+    $.ajax({ 
+        url: `/api/education/${edu.id}`,
+        method: 'POST',
+        data: edu
+    })
+);
---
+++ frontend/utils/job.js
@@ -0,0 +1,27 @@
+export const fetchJobs= ()=> (
+    $.ajax({ 
+        url: '/api/jobs'
+    })
+); 
+
+export const fetchJob=(jobId)=> ( 
+    $.ajax({ 
+        url: `/api/jobs/${jobId}`
+    })
+); 
+
+export const createJob= (job)=> ( 
+    $.ajax({ 
+        url: '/api/job',
+        method: 'POST',
+        data: job
+    })
+);
+
+export const updateEdu= (job)=> ( 
+    $.ajax({ 
+        url: `/api/job/${job.id}`,
+        method: 'POST',
+        data: job
+    })
+);
