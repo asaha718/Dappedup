1618e7e0a5c882a598f4cbe79afa3fa6ae445679
Anug Saha <anugsaha@gmail.com>
2021-05-20 23:58:31 -0400

    update styling of navebar

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -20,8 +20,11 @@ const Greeting = ({ currentUser, logout }) => {
       <div className="profile-btn">
         <Link to="/profile"><i className="fa fa-user"></i>Profile</Link>
       </div>
-      <h2 className="header-name">{currentUser.full_name}</h2> 
-      <button className="header-button" onClick={logout}>Log Out</button>
+      <div className="header-name">
+        <img src="https://i.pinimg.com/originals/18/d9/e1/18d9e1307018dbc76750ca7d5124fccd.png"/> 
+        <h2 className="name">{currentUser.full_name}</h2>
+      </div>
+      <button className="header-button" onClick={logout}><i class="fa fa-sign-out"></i>Sign out</button>
     </hgroup>
   );
 
