8c6d838ccc9de21623a8c7e60a1b05a74ce684d1
Anug Saha <anugsaha@gmail.com>
2021-07-05 16:50:18 -0400

    remove excess

--- frontend/components/feed/user_sidecard_container.js
+++ frontend/components/feed/user_sidecard_container.js
@@ -3,7 +3,6 @@ import { fetchCurrentUserFollows } from '../../actions/follow_actions';
 import UserSideCard from './user_sidecard'
 
 const mSTP= (state) => { 
-    console.log(state)
     return { 
     profile: state.entities.users[state.session.id], 
     following: state.entities.follows.current.followings
@@ -12,4 +11,5 @@ const mSTP= (state) => {
 const mDTP = dispatch => ({ 
     fetchCurrentUserFollows: ()=> dispatch(fetchCurrentUserFollows())
 })
+
 export default connect(mSTP, mDTP)(UserSideCard)
--- frontend/reducers/entities_reducer.jsx
+++ frontend/reducers/entities_reducer.jsx
@@ -5,7 +5,6 @@ import postsReducer from './posts_reducer';
 import educationReducer from './edus_reducer'; 
 import jobReducer from './jobs_reducer'; 
 import profilesReducer from "./profile_reducer";
-// import connectionReducer from "./connections_reducer";
 import followsReducer from "./follows_reducer";
 
 const entitiesReducer = combineReducers({
@@ -15,7 +14,6 @@ const entitiesReducer = combineReducers({
   jobs: jobReducer, 
   profiles: profilesReducer, 
   follows: followsReducer
-  // connections: connectionReducer
 });
 
 export default entitiesReducer;
