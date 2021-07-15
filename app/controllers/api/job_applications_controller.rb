38fcaa5933f542e840e8adad826a0bec3d443736
Anug Saha <anugsaha@gmail.com>
2021-07-15 18:02:59 -0400

    job app controller

--- app/controllers/api/job_applications_controller.rb
+++ app/controllers/api/job_applications_controller.rb
@@ -1,2 +1,21 @@
 class Api::JobApplicationsController < ApplicationController
+    def create
+        @user= User.find_by(id: current_user.id)
+        @job_app= @user.job_applications.new(job_app_params)
+
+        if @job_app.save
+            render :show
+        else
+            render json:@job_app.errors.full_messages, status: 422
+        end
+
+    end
+
+    def show
+        render :show
+    end
+
+    def job_app_params
+        params.render(:job_application).permit(:job_post_id)
+    end
 end
