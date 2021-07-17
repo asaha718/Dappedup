0442139fc2890c914c043c94cddb1e3b2cb89d84
Anug Saha <anugsaha@gmail.com>
2021-07-17 11:36:19 -0400

    have users apply to job posts

--- app/controllers/api/job_applications_controller.rb
+++ app/controllers/api/job_applications_controller.rb
@@ -1,13 +1,21 @@
 class Api::JobApplicationsController < ApplicationController
     def create
-        @user= User.find_by(id: current_user.id)
-        @job_app= @user.job_applications.new(job_app_params)
-        
-        if @job_app.save
-            render :show
+        # @user= User.find_by(id: current_user.id)
+
+        # @job_app= @user.job_applications.new(job_app_params)
+        if(current_user.user_role == 'company')
+            render "Company can not apply to position"
         else
-            render json:@job_app.errors.full_messages, status: 422
+            @job_app= JobApplication.new(job_post_id: params[:job_application], applicant_id: current_user.id)
+
+            if @job_app.save!
+                render :show
+            else
+                render json:@job_app.errors.full_messages, status: 422
+            end
         end
+
+
 
     end
 
@@ -15,7 +23,4 @@ class Api::JobApplicationsController < ApplicationController
         render :show
     end
 
-    def job_app_params
-        params.require(:job_application).permit(:job_post_id)
-    end
 end
