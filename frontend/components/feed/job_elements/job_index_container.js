627dfa271cdd7c4f622cb77a83a067ca6db847b3
Anug Saha <anugsaha@gmail.com>
2021-08-15 15:12:32 -0400

    beautify edu and job containers

--- frontend/components/feed/job_elements/job_index_container.js
+++ frontend/components/feed/job_elements/job_index_container.js
@@ -1,19 +1,24 @@
-import { connect } from 'react-redux';
-import {orderItems} from '../../../utils/selector';
+import { connect } from "react-redux";
+import { orderItems } from "../../../utils/selector";
 
-import { fetchAllJobs, updateJob, deleteJob } from '../../../actions/job_actions'; 
-import JobIndex from './job_index'; 
+import {
+  fetchAllJobs,
+  updateJob,
+  deleteJob,
+} from "../../../actions/job_actions";
+import JobIndex from "./job_index";
 
-const mSTP= (state, ownProps) =>{ 
-    return { 
-    jobs: orderItems(state.entities.jobs), 
-    currentUserId: state.session.id, 
-}}; 
+const mSTP = (state) => {
+  return {
+    jobs: orderItems(state.entities.jobs),
+    currentUserId: state.session.id,
+  };
+};
 
-const mDTP= dispatch => ({ 
-    fetchJobs: ()=> dispatch(fetchAllJobs()), 
-    updateJob: (job)=> dispatch(updateJob(job)), 
-    deleteJob: (job)=> dispatch(deleteJob(job))
-})
+const mDTP = (dispatch) => ({
+  fetchJobs: () => dispatch(fetchAllJobs()),
+  updateJob: (job) => dispatch(updateJob(job)),
+  deleteJob: (job) => dispatch(deleteJob(job)),
+});
 
-export default connect(mSTP, mDTP)(JobIndex); 
+export default connect(mSTP, mDTP)(JobIndex);
--- frontend/components/profile/edu_elements/edu_index_container.js
+++ frontend/components/profile/edu_elements/edu_index_container.js
@@ -1,20 +1,24 @@
-import { connect } from 'react-redux';
-import {orderItems} from '../../../utils/selector';
+import { connect } from "react-redux";
+import { orderItems } from "../../../utils/selector";
 
-import { fetchAllEdu, updateEdu, deleteEdu } from '../../../actions/edu_actions'; 
-import EduIndex from './edu_index'; 
+import {
+  fetchAllEdu,
+  updateEdu,
+  deleteEdu,
+} from "../../../actions/edu_actions";
+import EduIndex from "./edu_index";
 
-const mSTP= (state) =>{
-    return { 
-    educations: orderItems(state.entities.educations), 
-    currentUserId: state.session.id, 
-    }
-}; 
+const mSTP = (state) => {
+  return {
+    educations: orderItems(state.entities.educations),
+    currentUserId: state.session.id,
+  };
+};
 
-const mDTP= dispatch => ({ 
-    fetchEdus: ()=> dispatch(fetchAllEdu()), 
-    updateEdu: (edu)=> dispatch(updateEdu(edu)), 
-    deleteEdu: (edu)=> dispatch(deleteEdu(edu))
-})
+const mDTP = (dispatch) => ({
+  fetchEdus: () => dispatch(fetchAllEdu()),
+  updateEdu: (edu) => dispatch(updateEdu(edu)),
+  deleteEdu: (edu) => dispatch(deleteEdu(edu)),
+});
 
-export default connect(mSTP, mDTP)(EduIndex); 
+export default connect(mSTP, mDTP)(EduIndex);
